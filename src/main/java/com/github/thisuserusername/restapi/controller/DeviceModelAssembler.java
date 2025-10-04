8130839c36984d29947209ebcdec4624e80c5235
Vitalii Sushko <68609027+this-user-username@users.noreply.github.com>
2025-10-04 21:43:50 +0200

    Add missing links and manual test suite

--- src/main/java/com/github/thisuserusername/restapi/controller/DeviceModelAssembler.java
+++ src/main/java/com/github/thisuserusername/restapi/controller/DeviceModelAssembler.java
@@ -3,10 +3,14 @@ package com.github.thisuserusername.restapi.controller;
 import com.github.thisuserusername.restapi.dto.DeviceDTO;
 import com.github.thisuserusername.restapi.model.Device;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
 import java.time.format.DateTimeFormatter;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
 public class DeviceModelAssembler extends RepresentationModelAssemblerSupport<Device, DeviceDTO> {
@@ -24,6 +28,12 @@ public class DeviceModelAssembler extends RepresentationModelAssemblerSupport<De
         deviceModel.setBrand(entity.getBrand());
         deviceModel.setCreationTime(entity.getCreatedOn().format(DateTimeFormatter.ISO_DATE_TIME));
         deviceModel.setState(entity.getState().name().toLowerCase());
+
+        Link selfRel = linkTo(methodOn(DeviceController.class).getDevice(deviceModel.getId())).withSelfRel();
+        deviceModel.add(selfRel);
+        Link devicesLink = linkTo(methodOn(DeviceController.class).getAllDevices(null, null, 0, 100)).withRel("devices");
+        deviceModel.add(devicesLink);
+
         return deviceModel;
     }
 }
